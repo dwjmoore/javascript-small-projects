3d60b079a58232e2dbf65945cf3d31372b1a1c87
David Moore <dwjmoore@gmail.com>
2023-02-04 04:14:00 +0000

    Added functionality to show and hide the loading animation and to disable and enable to get quote button.

--- scripts/random-quote-generator.js
+++ scripts/random-quote-generator.js
@@ -1,9 +1,14 @@
+const spinner = document.querySelector('#js-spinner');
+
 const newQuoteButton = document.querySelector('#js-new-quote');
 newQuoteButton.addEventListener('click', getQuote);
 
 const endpoint = 'https://random-quote-api.dwjmoore.repl.co/random-quote';
 
 async function getQuote() {
+	spinner.classList.remove('hidden');
+	newQuoteButton.disabled = true;
+	
 	try {
 		const response = await fetch(endpoint);
 		if (!response.ok) {
@@ -11,9 +16,11 @@ async function getQuote() {
 		}
 		const json = await response.json();
 		displayQuote(json.quote);
-	} catch (err) {
-		console.log(err);
+	} catch {
 		alert('Failed to fetch new quote. Try again. The server might still be waking up.')
+	} finally {
+		newQuoteButton.disabled = false;
+		spinner.classList.add('hidden');
 	}
 }
 
