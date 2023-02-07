cd5e1e84b54cb618e20f267eb6a8bce5a5b3d564
David Moore <dwjmoore@gmail.com>
2023-02-07 13:46:47 +0000

    Created search function and calling it from submit function. Logging results to the console.

--- scripts/wikipedia-search-app.js
+++ scripts/wikipedia-search-app.js
@@ -5,5 +5,22 @@ function handleSubmit(event) {
 	event.preventDefault();
 	const inputValue = document.querySelector('.js-search-input').value;
 	const searchQuery = inputValue.trim();
-	console.log(searchQuery);
+
+	try {
+		const results = searchWikipedia(searchQuery);
+		console.log(results);
+	} catch (err) {
+		console.log(err);
+		alert('Failed to search Wikipedia.');
+	}
+}
+
+async function searchWikipedia(searchQuery) {
+	const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&stlimit=20&srsearch=${searchQuery}`;
+	const response = await fetch(endpoint);
+	if (!response.ok) {
+		throw Error(response.statusText);
+	}
+	const json = await response.json();
+	return json;
 }
