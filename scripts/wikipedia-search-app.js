2a3c670bf0d42124a68da019fe2478e04d3f0e65
David Moore <dwjmoore@gmail.com>
2023-02-08 00:15:17 +0000

    Created const variable for api endpoint and created a generic params object.

--- scripts/wikipedia-search-app.js
+++ scripts/wikipedia-search-app.js
@@ -3,26 +3,39 @@ const input = document.querySelector('#input');
 const errorSpan = document.querySelector('#error');
 const resultsContainer = document.querySelector('#results');
 
+const endpoint = 'https://en.wikipedia.org/w/api.php?';
+const params = {
+	origin: '*',
+	format: 'json',
+	action: 'query',
+	prop: 'extracts',
+	exchars: 250,
+	exintro: true,
+	explaintext: true,
+	generator: 'search',
+	gsrlimit: 20
+};
+
 const disableUi = () => {
 	input.disabled = true;
 	submitButton.disabled = true;
-}
+};
 
 const enableUi = () => {
 	input.disabled = false;
 	submitButton.disabled = false;
-}
+};
 
 const clearPreviousResults = () => {
 	resultsContainer.innerHTML = '';
 	errorSpan.innerHTML = '';
-}
+};
 
 const isInputEmpty = (input) => {
 	if (!input || input === '') return true;
 	return false;
-}
+};
 
 const showError = (error) => {
 	errorSpan.innerHTML = `&#128680 ${error} &#128680`;
-}
+};
