a03f3fe16fabf4087b2ac61a43c1a2b0c2a9a41e
David Moore <dwjmoore@gmail.com>
2023-02-09 23:12:39 +0000

    Removed duplicate setTweetButton() in quote generator. The function doesn't set the attributes after hitting the get quote button after receiving the error for the api not being awake yet. The tweet button has to be clicked twice on the first go to work.

--- scripts/random-quote-generator.js
+++ scripts/random-quote-generator.js
@@ -17,7 +17,6 @@ async function getQuote() {
 		const json = await response.json();
 		displayQuote(json.quote);
 		setTweetButton(json.quote);
-		setTweetButton(json.quote);
 	} catch {
 		alert("The random quote api is waking up. Click the 'Generate a new quote' button.");
 	} finally {
