86e12f0ba8367a0d03bf13a75ebb9656b9ad0b1e
David Moore <dwjmoore@gmail.com>
2023-02-12 23:36:15 +0000

    Added renderToDo function to render to-do items to UI. Still need to style the to-do list section.

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -8,7 +8,27 @@ const addToDo = (text) => {
 	};
 
 	toDoItems.push(toDo);
-	console.log(toDoItems);
+	renderToDo(toDo);
+}
+
+const renderToDo = (toDo) => {
+	const list = document.querySelector('#todo-list');
+	const isChecked = toDo.checked ? 'done' : '';
+	const node = document.createElement("li");
+
+	node.setAttribute('class', `todo-item ${isChecked}`);
+	node.setAttribute('data-key', toDo.id);
+
+	node.innerHTML = `
+ 		<input id="${toDo.id}" type="checkbox" />
+		<label for="${toDo.id}" class="tick"></label>
+		<span>${toDo.text}</span>
+		<button class="delete-todo">
+			<i class="fa-regular fa-circle-xmark"></i>
+		</button>
+ 	`;
+
+	list.append(node);
 }
 
 const form = document.querySelector('.todo-input-form');
