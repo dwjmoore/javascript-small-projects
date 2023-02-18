98d1f8afa72ad5c6e613a7be64ece02ae67e583f
David Moore <dwjmoore@gmail.com>
2023-02-18 23:19:50 +0000

    finished js for clock

---
+++ scripts/clock.js
@@ -0,0 +1,22 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+const setDate = () => {
+	const now = new Date();
+
+	const seconds = now.getSeconds();
+	const secondsDegrees = ((seconds / 60) * 360) + 90;
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+	const minutes = now.getMinutes();
+	const minutesDegrees = ((minutes / 60) * 360) + 90;
+	minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+	const hours = now.getHours() > 12 ? now.getHours() - 12 : now.getHours;
+	const hoursDegrees = ((hours / 12) * 360) + 90;
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+	console.log(hours);
+}
+
+setInterval(setDate, 1000);
